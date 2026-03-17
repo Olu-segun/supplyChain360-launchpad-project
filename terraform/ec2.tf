10b68a27b998ede89334e4e8b6c0b8421e4a7344
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2026-03-17 12:25:59 +0100

    Updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
-# ami access
-data "aws_ssm_parameter" "amazon_linux" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-}
+#  ami access
+# data "aws_ssm_parameter" "amazon_linux" {
+#   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+# }
 
 # Public key
 resource "aws_key_pair" "supplychain360_key" {
@@ -11,7 +11,7 @@ resource "aws_key_pair" "supplychain360_key" {
 
 # Create ec2 instance
 resource "aws_instance" "supplychain360_ec2" {
-  ami           = data.aws_ssm_parameter.amazon_linux.value
+  ami           = "ami-0c17cb8e234335014"
   instance_type = var.instance_type
   key_name      = aws_key_pair.supplychain360_key.key_name
   subnet_id = aws_subnet.public_subnet.id
